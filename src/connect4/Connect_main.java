b90e23140502d95344bee6186675d620d8cee830
snehitha <sm1552@wildcats.unh.edu>
2020-11-15 02:51:38 -0500

    Created class of main type

---
+++ src/connect4/Connect_main.java
@@ -0,0 +1,11 @@
+package connect4;
+
+public class Connect_main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("WELCOME TO CONNECT 4 GAME");
+		start s = new start();
+	}
+
+}
